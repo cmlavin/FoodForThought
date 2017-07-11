e5841c1bacdefac3ea3bd5b8bac1d107732b31d8
github username <github email address>
2017-07-11 11:03:04 -0400

    Added ingredient controller actions and views

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,12 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
-  
+
+
+  @@allergy_types = ["peanuts", "shellfish", "dairy", "egg", "soy", "wheat", "tree nuts", "corn", "garlic"]
+
+  def self.allergy_types
+    @@allergy_types
+  end
+
 end
