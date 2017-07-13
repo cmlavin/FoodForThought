f165da67e0da9f176c97ef618803902f43739e6a
Catherine <cmlavin7@gmail.com>
2017-07-13 14:43:22 -0400

    added search recipes function

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,9 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   belongs_to :author, class_name: "User", foreign_key: "author_id"
-  
+
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%")
+  end
+
 end
