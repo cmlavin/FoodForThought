1d92c877f253ebc2e3af6422bd3ffbca74b432e3
github username <github email address>
2017-07-14 09:27:54 -0400

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,7 @@ Rails.application.routes.draw do
 
   get '/', to: 'static#index', as: 'home'
 
+  get '/recipes/search', to: 'recipes#search', as: "search_recipes"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
