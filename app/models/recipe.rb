549acc58f9ecac75b96e9ca9fb2b1ce051523902
Catherine <cmlavin7@gmail.com>
2017-07-14 11:59:35 -0400

    validations updates

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,16 +5,9 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   validates :name, :instruction, :difficulty, presence: true
-  validate :more_than_one_ingredient
 
   def self.search(search)
     where("name LIKE ?", "%#{search}%")
-  end
-
-  def more_than_one_ingredient
-    if recipe.ingredients.length <= 1
-      errors.add(:ingredient, "Input at least 2 ingredients")
-    end
   end
 
 end
