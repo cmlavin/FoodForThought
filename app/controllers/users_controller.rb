cd8b39e5cb24ff4c0296ebf92462e0039e00ffd5
cheungth25 <cheungth25@gmail.com>
2017-07-18 09:36:18 -0400

    fixed password_confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:user_name, :password, :first_name, :last_name)
+    params.require(:user).permit(:user_name, :password, :password_confirmation , :first_name, :last_name)
   end
 
 end
