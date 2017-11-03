e7bda4b3a0ab672f74985680e83418e16f244a6b
Catherine <cmlavin7@gmail.com>
2017-11-03 16:07:50 -0400

    Renaming methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?, :current_ingredients, :current_ingredients_hash, :handle_dem_checked_boxes, :handle_dem_unchecked_boxes, :current_recipes
+  helper_method :current_user, :logged_in?, :current_ingredients, :current_ingredients_hash, :handle_the_checked_boxes, :handle_the_unchecked_boxes, :current_recipes
 
   def current_user
     if session[:user_id]
@@ -32,24 +32,24 @@ class ApplicationController < ActionController::Base
     session[:recipes_list] ||= []
   end
 
-  def handle_dem_unchecked_boxes(dem_boxes)
-    format_dem_boxes = current_ingredients.select do |box|
-      !dem_boxes.include?(box.to_s)
+  def handle_the_unchecked_boxes(the_boxes)
+    format_the_boxes = current_ingredients.select do |box|
+      !the_boxes.include?(box.to_s)
     end
-    format_dem_hash = current_ingredients_hash.select do |hash|
-      !dem_boxes.include?(hash["id"].to_s)
+    format_the_hash = current_ingredients_hash.select do |hash|
+      !the_boxes.include?(hash["id"].to_s)
     end
     session.delete(:ingredients_list)
     session.delete(:ingredients_hash)
-    session[:ingredients_hash] = format_dem_hash
-    session[:ingredients_list] = format_dem_boxes
+    session[:ingredients_hash] = format_the_hash
+    session[:ingredients_list] = format_the_boxes
   end
 
-  def handle_dem_checked_boxes(dem_boxes, collection)
-    format_dem_boxes = dem_boxes[1..-1].select do |box|
+  def handle_the_checked_boxes(the_boxes, collection)
+    format_the_boxes = the_boxes[1..-1].select do |box|
       collection.include?(box.to_i)
     end
-    format_dem_boxes.map { |box| box.to_i}
+    format_the_boxes.map { |box| box.to_i}
   end
 
   def handle_unchecked_recipe_boxes(boxes)
